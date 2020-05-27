692a349c1cb6f3be2b42b8e7ae847646db2d0f1b
Yong Hoon Shin <zcabyhs@ucl.ac.uk>
2020-05-27 13:49:19 +0900

    chore: add stylelint, prettier, tsc, webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,40 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.tsx",
-  devtool: "source-map",
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-      chunkFilename: "styles.css",
-    }),
-    new HtmlWebpackPlugin({
-      template: "src/templates/index.html",
-    }),
-  ],
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].[contenthash].js",
-    publicPath: "/",
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
-      },
+    entry: "./src/index.tsx",
+    devtool: "source-map",
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+            chunkFilename: "styles.css",
+        }),
+        new HtmlWebpackPlugin({
+            template: "src/templates/index.html",
+        }),
     ],
-  },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "[name].[contenthash].js",
+        publicPath: "/",
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                ],
+            },
+        ],
+    },
 };
